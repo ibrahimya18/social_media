5194ab21caee15d8b306247dc1cc46356699dce7
ibrahim_samed <IbrahimSamed.Yalcin@vestel.com.tr>
2025-04-07 15:57:25 +0300

    some codes added

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import { register } from "./controllers/auth.js";
 
 /* CONFİGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +36,12 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+/* Routes with  files */
+app.post("/auth/regiter", upload.single("picture"),register);
+
+/* ROUTES*/
+app.use("/auth", authRoutes);
+app.use("/users",userRoutes)
 /*MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
