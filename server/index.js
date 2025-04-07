8aa38b6e5cf7bb102fb52fb8b8d6b4fdcf764a50
ibrahim_samed <IbrahimSamed.Yalcin@vestel.com.tr>
2025-04-07 16:55:47 +0300

    added backend codes

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/post.js"
 import { register } from "./controllers/auth.js";
+import {createPost} from "./controllers/posts.js"
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFİGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -38,10 +41,12 @@ const upload = multer({ storage });
 
 /* Routes with  files */
 app.post("/auth/regiter", upload.single("picture"),register);
-
+app.post("/posts",verifyToken,upload.single("picture"), createPost)
 /* ROUTES*/
 app.use("/auth", authRoutes);
-app.use("/users",userRoutes)
+app.use("/users",userRoutes),
+app.use("/posts",postRoutes)
+
 /*MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
