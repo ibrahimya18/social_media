8fa573f06d5e35a096bf74ff117a4e3eb10a3d6b
ibrahim_samed <IbrahimSamed.Yalcin@vestel.com.tr>
2025-04-08 15:38:19 +0300

    begining frontend

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import {BrowserRouter,Navigate,Routes,Route} from "react-router-dom";
+import HomePage from "./scenes/homePage";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+import { useMemo } from "react";
+import {useSelector} from "react-redux";
+import {Cssbasaline,ThemeProvider} from "@mui/icons-material";
+import {createTheme} from "@mui/material/styles";
+import { themeSettings } from "./theme";
+
+function App() {
+  const mode = useSelector((state)=>state.mode);
+  const theme=useMemo(()=>createTheme(themeSettings(mode)),[mode]);
+
+
+  return <div className="App">
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Cssbasaline/>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/profile/:userId" element={<ProfilePage/>}/>
+      </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
+  </div>;
+}
+
+export default App;
